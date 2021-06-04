39d0d856f960b1ab7dfd6a5dd692426f878636e6
Aurora <ada10016@yahoo.com>
2021-06-04 15:48:10 -0400

    using while loop

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-var btn = document.querySelector('button');
+
+const btn = document.querySelector('button');
+    
+        // b = new Array(2, 4, 6, 8, 10, 12),
+        // i = new Array(14, 16, 18, 20, 22),
+        // n = new Array(22, 24, 26, 28, 30),
+        // g = new Array(32, 34, 36, 38, 40),
+        // o = new Array(42, 44, 46, 48, 50);
+
+const myh3 = document.querySelector("h3");
 
 
-const myh3 = document.querySelector('h3')
-
-
-function changeNum() {
-const random = Math.floor(Math.random() * 50 ) + 1;
-
-for(let i =0; i < 50; i++)
-    myh3.textContent = random;
-    myh3.style.color='red';
-    myh3.style.fontSize='54px';
+for(let i = 0; i <= 50;i += 2) {
+  myh3.style.color = "red";
+  myh3.style.fontSize = "54px";
+  myh3.textContent = i;
+  
+  console.log(i);
 }
-
-
-
-  // b = new Array(2, 4, 6, 8, 10, 12),
-  // i = new Array(14, 16, 18, 20, 22),
-  // n = new Array(22, 24, 26, 28, 30),
-  // g = new Array(32, 34, 36, 38, 40),
-  // o = new Array(42, 44, 46, 48, 50) 
