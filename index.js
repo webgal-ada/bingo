7ae1bae9ccf0cf194f8a7d46c22b6f806d3c3608
Aurora <ada10016@yahoo.com>
2021-06-05 12:59:16 -0400

    letter changes ok, number displays but is stagnant

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
+const letter = ["B", "I", "G", "N", "O", "G", "B", "N"];
+const btn = document.querySelector("button");
 
-const btn = document.querySelector('button');
-    
-        // b = new Array(2, 4, 6, 8, 10, 12),
-        // i = new Array(14, 16, 18, 20, 22),
-        // n = new Array(22, 24, 26, 28, 30),
-        // g = new Array(32, 34, 36, 38, 40),
-        // o = new Array(42, 44, 46, 48, 50);
+// b = new Array(2, 4, 6, 8, 10, 12),
+// i = new Array(14, 16, 18, 20, 22),
+// n = new Array(22, 24, 26, 28, 30),
+// g = new Array(32, 34, 36, 38, 40),
+// o = new Array(42, 44, 46, 48, 50);
 
-const myh3 = document.querySelector("h3");
-
-
-for(let i = 0; i <= 50;i += 2) {
-  myh3.style.color = "red";
-  myh3.style.fontSize = "54px";
-  myh3.textContent = i;
-  
-  console.log(i);
+function changeLetter() {
+  let randomLetter = Math.floor(Math.random() * letter.length);
+  document.querySelector("h3").innerText = letter[randomLetter];
+  console.log(letter[randomLetter]);
 }
+let i = 0;
+while (i <= 50) {
+  console.log(i);
+  i += 2;
+  document.querySelector("span").innerText = [i];
+}
+
+//let randomNumb = Math.floor(Math.random() * 50) ;
+
+// document.querySelector('span').innerText = [randomNumb];
+
+btn.addEventListener("click", changeLetter());
+// let i = 0;
+// while (i <= 50) {
+//   console.log(i);
+//   i += 2;
+// }
